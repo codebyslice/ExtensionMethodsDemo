b022532a36f28944e1ac9295cd3eca0c3f7f1970
Anirudh Samudrala <anirudh.enigma@gmail.com>
2019-03-31 02:00:46 -0700

    Add project files.

---
+++ ExtensionMethods/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ExtensionMethods.Models;
+using ExtensionMethods.Extensions;
+
+namespace ExtensionMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public bool IsEmail(string str)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+                return false;
+            return str.IsEmail();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
